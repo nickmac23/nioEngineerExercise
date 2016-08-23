119234385b18de821ea17a6c7fb05690048dd35d
Nick MacIntyre <nickmac@Nick-MacBook-Air.local>
2016-08-23 13:36:42 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+var Analytics = class {
+  constructor(socketHost, fruitQuantity, shoppers, divId) {
+    this.socket = io.connect(socketHost)
+    this.divId = divId
+    this.fruitCount = fruitQuantity;
+    this.shopperCount = shoppers;
+  }
+
+  openStream(){
+    this.socket.emit('ready', 'groceries')
+    this.listenStream()
+  }
+
+  listenStream(){
+    this.socket.on("recvData", (data) => {
+      this.saveData(JSON.parse(data).cart)
+      this.upDateView()
+    })
+  }
+
+  saveData(cart){
+    this.shopperCount++
+    cart.map(item => {
+      if(item.type === 'fruit') this.fruitCount++
+    })
+  }
+
+  analytics() {
+    var analytics = {
+      aveFruitPerShopper: this.fruitCount/this.shopperCount
+    }
+    return analytics
+  }
+
+  upDateView() {
+    var analytics = this.analytics()
+    document.getElementById(this.divId).innerHTML = '<h1> mean fruitConunt/shopperCount: ' + analytics.aveFruitPerShopper + '</h1>'
+  }
+
+};
+
+var cart = new Analytics('http://brand.nioinstances.com',0,0, 'mean')
+cart.openStream()
---
+++ readme.md
@@ -0,0 +1,1 @@
+clone, open index.html
